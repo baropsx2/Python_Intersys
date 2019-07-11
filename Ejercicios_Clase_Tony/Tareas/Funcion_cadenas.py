85ed47c95106e30737bf8a71442013bdee3e4b2a
David Barocio <baropsx2@gmail.com>
2019-07-11 15:42:47 -0500

    Nuevos cambios, 11 Jul 2019

---
+++ Ejercicios_Clase_Tony/Tareas/Funcion_cadenas.py
@@ -0,0 +1,12 @@
+'''
+Crear una función que se llame cadenas, la función va a recibir una lista y debe devolver una lista de cadenas
+que contenga la lista original.
+Ejemplo:
+cadenas([1, 2, 3, “hola”, 0, “mundo”]) ---> [“hola”, “mundo”]
+cadenas([1, 2]) ---> []
+cadenas([“hola”, 23, 56, “frfr”]) ---> [“hola”, “frfr”]
+'''
+
+
+
+def cadenas(lista)
